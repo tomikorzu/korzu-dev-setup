f8113549e0490a7a6a314fb58cdbf649d82a30ba
tomikorzu <tomas.korzusehec@gm2dev.com>
2026-09-16 01:59:31 -0300

    feat: add @mui/material-nextjs package and integrate AppRouterCacheProvider into ThemeProvider for improved theming and caching support

--- src/modules/shared/providers/Theme.provider.tsx
+++ src/modules/shared/providers/Theme.provider.tsx
@@ -3,13 +3,16 @@
 import type { ReactNode } from "react";
 import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import theme from "@/theme/theme";
 
 export default function ThemeProvider({ children }: { children: ReactNode }) {
   return (
-    <MUIThemeProvider theme={theme}>
-      <CssBaseline enableColorScheme />
-      {children}
-    </MUIThemeProvider>
+    <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+      <MUIThemeProvider theme={theme}>
+        <CssBaseline enableColorScheme />
+        {children}
+      </MUIThemeProvider>
+    </AppRouterCacheProvider>
   );
 }
